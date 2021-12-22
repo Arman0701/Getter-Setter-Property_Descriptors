b9585476b85f131515f4960188e310503af29af4
Arman773 <tadevosyan889@gmail.com>
2021-12-22 22:29:49 +0400

    fix: fixed issues

--- Required-Tasks/Task-2.js
+++ Required-Tasks/Task-2.js
@@ -3,18 +3,22 @@
 function getBestStudent(obj){
     let max = 0;
     let k = 0;
-    for (let score of Object.values(obj)){
-        let avgScore = score.reduce(( a,b )=>{ return a+b })/score.length;
+    let arr = Object.values(obj);
+    for (let i=0; i<arr.length; i++){
+        let avgScore = arr[i].reduce((a,b)=>{ return a+b })/arr[i].length;
         if (max<avgScore){
-            if (max > 0) k++;
             max = avgScore;
+            k = i;
         }
     }
+    console.log(Object.keys(obj));
     return Object.keys(obj)[k];
 }
 
 console.log(getBestStudent({
-    Bob: [100, 70, 80],
-    John: [100, 90, 80],
+    Gago: [100, 100, 100],
+    Hakob: [100, 110, 112],
     Arman: [50, 50, 50],
+    John: [100, 90, 80],
+    Bob: [100, 70, 80],
 }));
